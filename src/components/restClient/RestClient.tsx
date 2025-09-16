12ad44950ad104d420c8ed6bf69525d29a8eeff7
vahagn85 <v.ulikhanyan@yandex.ru>
2025-09-16 23:22:19 +0200

    feat: implement util variables for insert url,body,header

--- src/components/restClient/RestClient.tsx
+++ src/components/restClient/RestClient.tsx
@@ -5,6 +5,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import { RestForm } from '@/types/rest.type';
 import RestBar from './RestBar';
 import RestHeaders from './RestHeaders';
+import { replaceVariables } from '@/utils/variables';
 
 function RestClient() {
   const { register, handleSubmit, control, watch, setValue } =
@@ -25,46 +26,52 @@ function RestClient() {
   const onSubmit = async (data: RestForm) => {
     try {
       console.log(data);
+      const testUrl = '{{Url}}/end/{{TXT}}/1';
+      const replaceText = replaceVariables(testUrl);
+      console.log(replaceText);
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="space-y-4 max-w-4xl mx-auto"
-    >
-      <RestBar register={register} />
+    <>
+      <h1 className="text-3xl font-semibold text-center mb-4">REST Client</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-4 max-w-4xl mx-auto"
+      >
+        <RestBar register={register} />
 
-      <Tabs defaultValue="headers" className="mt-4">
-        <TabsList className="w-full">
-          <TabsTrigger value="headers">Headers</TabsTrigger>
-          <TabsTrigger value="body">Body</TabsTrigger>
-        </TabsList>
+        <Tabs defaultValue="headers" className="mt-4">
+          <TabsList className="w-full">
+            <TabsTrigger value="headers">Headers</TabsTrigger>
+            <TabsTrigger value="body">Body</TabsTrigger>
+          </TabsList>
 
-        <TabsContent
-          value="headers"
-          className="border border-gray-200 rounded px-4 pb-4 shadow-xl"
-        >
-          <RestHeaders
-            register={register}
-            fields={fields}
-            append={append}
-            remove={remove}
-            watch={watch}
-            setValue={setValue}
-          />
-        </TabsContent>
+          <TabsContent
+            value="headers"
+            className="border border-gray-200 rounded px-4 pb-4 shadow-xl"
+          >
+            <RestHeaders
+              register={register}
+              fields={fields}
+              append={append}
+              remove={remove}
+              watch={watch}
+              setValue={setValue}
+            />
+          </TabsContent>
 
-        <TabsContent
-          value="body"
-          className="border border-gray-200 rounded px-4 pb-4 shadow-xl"
-        >
-          <p>BODY</p>
-        </TabsContent>
-      </Tabs>
-    </form>
+          <TabsContent
+            value="body"
+            className="border border-gray-200 rounded px-4 pb-4 shadow-xl"
+          >
+            <p>BODY</p>
+          </TabsContent>
+        </Tabs>
+      </form>
+    </>
   );
 }
 export default RestClient;
