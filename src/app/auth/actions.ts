6431b0f652f213630a980993f386beec9841daed
vahagn85 <v.ulikhanyan@yandex.ru>
2025-09-21 22:43:11 +0200

    refactor: update authform and action

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -4,39 +4,47 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 export async function loginAction(formData: FormData) {
-  const supabase = await createClient();
+  try {
+    const supabase = await createClient();
 
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
+    const data = {
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    };
 
-  const { error } = await supabase.auth.signInWithPassword(data);
+    const { error } = await supabase.auth.signInWithPassword(data);
 
-  if (error) {
-    return { error: true, message: error.message };
+    if (error) {
+      return { error: true, message: error.message };
+    }
+
+    revalidatePath('/', 'layout');
+    redirect('/');
+  } catch (err) {
+    return { error: true, message: (err as Error).message || 'Unknown error' };
   }
-
-  revalidatePath('/', 'layout');
-  redirect('/');
 }
 
 export async function signupAction(formData: FormData) {
-  const supabase = await createClient();
+  try {
+    const supabase = await createClient();
 
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
+    const data = {
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    };
 
-  const { error } = await supabase.auth.signUp(data);
+    const { error } = await supabase.auth.signUp(data);
 
-  if (error) {
-    return { error: true, message: error.message };
+    if (error) {
+      return { error: true, message: error.message };
+    }
+
+    revalidatePath('/', 'layout');
+    redirect('/');
+  } catch (err) {
+    return { error: true, message: (err as Error).message || 'Unknown error' };
   }
-
-  revalidatePath('/', 'layout');
-  redirect('/');
 }
 
 export async function signOutAction() {
